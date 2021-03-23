edc7e90d0080c6d95518d21fa5491858c18453ae
alistair3149 <alistair31494322@gmail.com>
2021-03-23 12:31:42 -0400

    feat: experimental VE support

---
+++ includes/HCaptchaHooks.php
@@ -0,0 +1,23 @@
+<?php
+
+class HCaptchaNoCaptchaHooks {
+	/**
+	 * Adds extra variables to the global config
+	 *
+	 * @param array &$vars Global variables object
+	 * @return bool Always true
+	 */
+	public static function onResourceLoaderGetConfigVars( array &$vars ) {
+		global $wgHCaptchaSiteKey;
+		//global $wgCaptchaClass;
+
+		//if ( $wgCaptchaClass === 'hCaptcha' ) {
+		$vars['wgConfirmEditConfig'] = [
+			'hCaptchaSiteKey' => $wgHCaptchaSiteKey,
+			'hCaptchaScriptURL' => 'https://hcaptcha.com/1/api.js'
+		];
+		//}
+
+		return true;
+	}
+}
